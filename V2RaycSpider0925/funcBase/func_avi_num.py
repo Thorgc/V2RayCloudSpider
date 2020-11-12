2c8823c010125d2dd514915ffad0696cd7388ba9
QIN2DIM <RmAlkaid@outlook.com>
2020-11-12 16:54:55 +0800

    .

--- V2RaycSpider0925/funcBase/func_avi_num.py
+++
@@ -1,5 +0,0 @@
-# windows10 下不可用
-from MiddleKey.VMes_IO import avi_num
-
-if __name__ == '__main__':
-    print(avi_num().strip())
--- V2RaycSpider0925/funcBase/get_ssr_link.py
+++
@@ -1,5 +0,0 @@
-# windows10 下不可用
-from spiderNest.defender import Defender
-
-if __name__ == '__main__':
-    Defender('ssr').run()
